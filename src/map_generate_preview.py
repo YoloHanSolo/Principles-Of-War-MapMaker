7dce156ff6d4da69a189ac1c2e34b2c2790cfcee
JanPelicon <jnpelicon@gmail.com>
2025-12-22 23:02:59 +0100

    map preview

--- src/map_generate_preview.py
+++ src/map_generate_preview.py
@@ -2,6 +2,7 @@ from os import listdir, path
 from json import load
 from inquirer import List, prompt
 from PIL import Image, ImageFont, ImageDraw
+from operator import add
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 ROOT_DIR = path.abspath(path.join(BASE_DIR, ".."))
@@ -9,6 +10,7 @@ MAPS_DIR = path.join(ROOT_DIR, "maps")
 MAPS_DATA_DIR = path.join(ROOT_DIR, "maps_data")
 RESOURCES_DIR = path.join(ROOT_DIR, "resources")
 
+HEXAGON_CORNERS = ((60, 0), (120, 33), (120, 107), (60, 140), (0, 107), (0, 33))
 
 class MapPreview:
 
@@ -23,19 +25,14 @@ class MapPreview:
         self.hexagon_h = 0
         self.pixel_w = 0
         self.pixel_h = 0
+        
+        self.hex_lookup = {}
 
         self.images = {"terrain": {}, "landmark": {}, "river": []}
-
-        self.river_map = [
-            [False, False, False, False, False, False],
-            [True, False, False, False, False, False],
-            [False, True, False, False, False, False],
-            [False, False, True, False, False, False],
-            [True, True, False, False, False, False],
-            [True, False, True, False, False, False],
-            [False, True, True, False, False, False],
-            [True, True, True, False, False, False],
-        ]
+        
+        self.canvas = None
+        self.draw = None
+        self.font = ImageFont.truetype("arial.ttf", 40)
 
     def load_terrain_images(self):
         for image_filename in listdir(path.join(RESOURCES_DIR, "terrain")):
@@ -67,72 +64,125 @@ class MapPreview:
         self.hexagon_h = data["metadata"]["height"]
         self.pixel_w = self.hexagon_w * 120 + 60
         self.pixel_h = self.hexagon_h * 107 + 33
+        
+        self.hex_lookup = {(h['x'], h['y']): h for h in self.hexagons}
+        self.canvas = Image.new("RGBA", (self.pixel_w, self.pixel_h), (0, 0, 0, 0))
+        self.draw = ImageDraw.Draw(self.canvas)
 
-    def generate_preview(self):
-
-        canvas = Image.new("RGBA", (self.pixel_w, self.pixel_h), (0, 0, 0, 0))
-
-        draw = ImageDraw.Draw(canvas)
-
-        font = ImageFont.truetype("arial.ttf", 40)
+    def draw_hexagons(self):
 
         for hexagon in self.hexagons:
             ix = hexagon["x"]
             iy = hexagon["y"]
             terrain = hexagon["terrain"]
             landmark = hexagon["landmark"]
-            river = hexagon["river"]
 
             px = ix * 120 + (iy % 2) * 60
             py = iy * 107
 
-            river_index = 0
+            terrain_img = self.images["terrain"][terrain]
+            landmark_img = self.images["landmark"][landmark]
 
-            for i in range(8):
-                if not all(x == y for x, y in zip(river, self.river_map[i])):
-                    continue
-                river_index = i
-                break
+            self.canvas.paste(terrain_img, (px, py), terrain_img)
 
-            canvas.paste(
-                self.images["terrain"][terrain],
-                (px, py),
-                self.images["terrain"][terrain],
-            )
+            if iy % 2 == 0 and ix + 1 == self.hexagon_w:
+                self.canvas.paste(terrain_img, (px + 120, py), terrain_img)    
+                
+            if iy % 2 == 1 and ix == 0:   
+                self.canvas.paste(terrain_img, (px - 120, py), terrain_img)    
+                
+            if iy == 0:
+                self.canvas.paste(terrain_img, (px - 60, py - 107), terrain_img)    
+                if ix + 1 == self.hexagon_w:               
+                    self.canvas.paste(terrain_img, (px + 60, py - 107), terrain_img)    
 
-            if landmark != "default":
-                canvas.paste(
-                    self.images["landmark"][landmark],
-                    (px, py),
-                    self.images["landmark"][landmark],
-                )
+            if iy + 1 == self.hexagon_h:
+                self.canvas.paste(terrain_img, (px - 60, py + 107), terrain_img)     
+                if ix + 1 == self.hexagon_w:               
+                    self.canvas.paste(terrain_img, (px + 60, py + 107), terrain_img)    
 
-            if river_index > 0:
-                canvas.paste(
-                    self.images["river"][river_index],
-                    (px, py),
-                    self.images["river"][river_index],
-                )
-
+            if landmark == "default":
+                continue
+            
+            self.canvas.paste(landmark_img, (px, py), landmark_img)   
+     
+    def draw_city_names(self):
         for city in self.cities:
             ix = city["x"]
             iy = city["y"]
             name = city["name"]
 
-            bbox = draw.textbbox((0, 0), name, font=font)
+            bbox = self.draw.textbbox((0, 0), name, font=self.font)
             text_w = bbox[2] - bbox[0]
             text_h = bbox[3] - bbox[1]
 
             px = ix * 120 + (iy % 2) * 60 + 60 - text_w // 2
             py = iy * 107
 
-            draw.rectangle(
+            self.draw.rectangle(
                 (px, py + 80, px + text_w, py + 80 + text_h + 10),
                 fill=(0, 0, 0, 255),
             )
-            draw.text((px, py + 80), name, fill=(255, 255, 255, 255), font=font)
+            self.draw.text((px, py + 80), name, fill=(255, 255, 255, 255), font=self.font)   
+         
+    def draw_rivers(self):
+                
+        for hexagon in self.hexagons:
+            ix = hexagon["x"]
+            iy = hexagon["y"]
+            river = hexagon["river"]
 
-        canvas.save(path.join(MAPS_DATA_DIR, self.map_id, "preview.png"))
+            px = ix * 120 + (iy % 2) * 60
+            py = iy * 107  
+            
+            for index, value in enumerate(river):
+                
+                if not value:
+                    continue
+                
+                (cpx1, cpy1) = HEXAGON_CORNERS[index]
+                (cpx2, cpy2) = HEXAGON_CORNERS[(index + 1) % 6]
+                
+                self.draw.line([(px + cpx1, py + cpy1), (px + cpx2, py + cpy2)], fill=(0, 0, 255), width=12)
+
+    def draw_railway(self):
+        for hexagon in self.hexagons:
+            ix = hexagon["x"]
+            iy = hexagon["y"]
+            railway = hexagon["railway"] 
+
+            if not railway:
+                continue
+
+            if iy % 2 == 0:
+                neighbor_coords = [(ix, iy-1), (ix+1, iy), (ix, iy+1), (ix-1, iy+1), (ix-1, iy), (ix-1, iy-1)]    
+            else:
+                neighbor_coords = [(ix+1, iy-1), (ix+1, iy), (ix+1, iy+1), (ix, iy+1), (ix-1, iy), (ix, iy-1)]
+
+            railways = [False, False, False, False, False, False]
+            for i, (nx, ny) in enumerate(neighbor_coords):
+                neighbor = self.hex_lookup.get((nx, ny))
+                if neighbor and neighbor["railway"]:
+                    railways[i] = True
+
+            px = ix * 120 + (iy % 2) * 60
+            py = iy * 107  
+
+            (cpx1, cpy1) = (60, 70)
+            
+            for index, value in enumerate(railways):
+                if not value:
+                    continue
+      
+                hc1 = HEXAGON_CORNERS[index]
+                hc2 = HEXAGON_CORNERS[(index + 1) % 6]
+      
+                (cpx2, cpy2) = ((hc1[0] + hc2[0]) / 2, (hc1[1] + hc2[1]) / 2)            
+            
+                self.draw.line([(px + cpx1, py + cpy1), (px + cpx2, py + cpy2)], fill=(0, 0, 0), width=12)
+                
+    def save_canvas(self):
+        self.canvas.save(path.join(MAPS_DATA_DIR, self.map_id, "preview.png"))
 
 
 def run():
@@ -148,4 +198,8 @@ def run():
     map_preview = MapPreview(map_id, 0)
     map_preview.load_terrain_images()
     map_preview.load_map_data()
-    map_preview.generate_preview()
+    map_preview.draw_hexagons()
+    map_preview.draw_rivers()
+    map_preview.draw_railway()
+    map_preview.draw_city_names()
+    map_preview.save_canvas()
